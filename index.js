1c5af5bee3809eac6ee64b881044b3a32b7f125f
Arnob3108 <sajedurrahmanarnob@gmail.com>
2022-11-23 02:45:06 +0600

    appointment function created

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.esqm07b.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const appointmentOptionsCollection = client
+      .db("doctorsPortal")
+      .collection("appointmentOptions");
+
+    const bookingsCollection = client
+      .db("doctorsPortal")
+      .collection("bookings");
+
+    app.get("/appointmentOptions", async (req, res) => {
+      const date = req.query.date;
+      console.log(date);
+      const query = {};
+      const options = await appointmentOptionsCollection.find(query).toArray();
+      const bookingQuery = { appointmentDate: date };
+      const allreadyBooked = await bookingsCollection
+        .find(bookingQuery)
+        .toArray();
+      options.forEach((option) => {
+        const optionBooked = allreadyBooked.filter(
+          (book) => book.treatmentName === option.name
+        );
+        const bookedSlots = optionBooked.map((book) => book.slot);
+        const remainingSlots = option.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        option.slots = remainingSlots;
+        console.log(option.name, bookedSlots);
+      });
+      res.send(options);
+    });
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        appointmentDate: booking.appointmentDate,
+        email: booking.email,
+        treatmentName: booking.treatmentName,
+      };
+      const allreadyBooked = await bookingsCollection.find(query).toArray();
+
+      if (allreadyBooked.length) {
+        const message = `you allready booked on ${booking.appointmentDate}`;
+        return res.send({ acknowledged: false, message });
+      }
+
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
+
+app.get("/", async (req, res) => {
+  res.send("doctors portal server is running");
+});
+
+app.listen(port, () => console.log("server running on", port));
